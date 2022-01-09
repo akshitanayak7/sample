617c487b5f65858a59d6aaacc222e92b32209db4
akshitanayak7 <97383772+akshitanayak7@users.noreply.github.com>
2022-01-09 15:57:45 +0530

    Add files via upload

---
+++ firstprogram.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    printf("Hello guys, I am Akshita Nayak from cs1.My friend Kalpana Vishwakarma from cs1 is sitting beside me.\n");
+    printf("We are making a simple program of printing some sentences on the screen.We hope ma'am like our little effort.\n");
+    printf("Bye guys. Thankyou.\n");
+    return (0);
+}
